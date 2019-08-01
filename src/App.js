f333263d2483d383e53be1378cfc94485bd7966e
dorukusumezbas <d.usumezbas@gmail.com>
2019-08-01 17:13:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ReactTable from 'react-table';
+import axios from 'axios';
+import 'react-table/react-table.css';
+import 'antd/dist/antd.css';
+import {Button, Typography} from 'antd';
+
+const {Title} = Typography;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default App;
+  const getData = () => {
+    setIsLoading(true);
+    axios.get('http://ec2-3-19-28-161.us-east-2.compute.amazonaws.com/panel/')
+      .then((response) => {
+      response.data.success ? setData(response.data.content) : setData([]);
+    }).then(() => {
+      setIsLoading(false);
+    });
+  }
+
+  const updateDatabase = () => {
+    setIsLoading(true)
+    axios.get('http://ec2-3-19-28-161.us-east-2.compute.amazonaws.com/autoupdate/')
+      .then((response) => {
+        response.data.success ? alert('Database updated.') : alert('Error: Couldnt update' +
+          ' database');
+      })
+      .then(() => {
+        setIsLoading(false);
+      })
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getDate = dateobject => dateobject.toLocaleDateString();
+  const getTime = dateobject => dateobject.toLocaleTimeString();
+  const columns = [
+    {
+      Header: 'Ürün Bilgileri',
+      columns: [
+        {
+          Header: 'EAN',
+          accessor: 'ean',
+        }, {
+          Header: 'SKU',
+          accessor: 'shopifysku',
+        }, {
+          Header: 'Renk',
+          accessor: 'shopifyrenk',
+        }, {
+          Header: 'Beden',
+          accessor: 'shopifybeden',
+        }],
+    },
+
+    {
+      Header: 'Stok Farkı',
+      columns: [
+        {
+          Header: 'Stok (Shopify)',
+          accessor: 'available',
+        },
+        {
+          Header: 'Stok (Nebim)',
+          accessor: 'nebimstok',
+        }],
+    }, {
+      Header: 'Stok Değişikliği',
+      columns: [
+        {
+          id: 'sondegisikliktarihi',
+          Header: 'Shopify Son Stok Değişikliği Günü',
+          accessor: d => getDate(new Date(d.updated_at)),
+        },
+        {
+          id: 'sondegisikliksaati',
+          Header: 'Shopify Son Stok Değişikliği Saati',
+          accessor: d => getTime(new Date(d.updated_at)),
+        }],
+    }];
+  const pageSizeOptions = [5, 10, 15, 20, 25, 50, 100]
+  return (
+      <div>
+        <div className='header'>
+          <Title style={{marginTop: 15}}>Shopify-Nebim Stok Entegrasyonu</Title>
+        </div>
+        <div className='header' style = {{marginBottom: 25}}>
+          <Button onClick = {() => getData()} loading = {isLoading} style={{marginRight: 30, marginLeft: 30}} type="default" size={"large"}>Veriyi Yenile</Button>
+          <Button onClick = {() => updateDatabase()} loading = {isLoading} style={{marginRight: 30, marginLeft: 30}} type="default" size={"large"}>Veritabanını
+            Güncelle</Button>
+          <Button loading = {isLoading} style={{marginRight: 30, marginLeft: 30}} type="danger" size={"large"}>Stoğu
+            Aktar</Button>
+        </div>
+        <ReactTable
+          className="-striped -highlight"
+          data={data}
+          columns={columns}
+          loading={isLoading}
+          filterable
+          pageSizeOptions= {pageSizeOptions}
+          defaultPageSize= {15}
+        />
+      </div>
+      );
+      }
+
+      export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './TablePage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
